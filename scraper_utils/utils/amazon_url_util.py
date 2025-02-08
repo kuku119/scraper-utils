42c7edfbb231d575122edf6a3f99b97d42de7620
Jiang <jdycty2002@163.com>
2025-02-08 17:56:11 +0800

    update amazon url util

--- scraper_utils/utils/amazon_url_util.py
+++ scraper_utils/utils/amazon_url_util.py
@@ -4,32 +4,40 @@
 
 from __future__ import annotations
 
-import re as __re
+import re
 from typing import TYPE_CHECKING
-from urllib.parse import quote_plus as __quote_plus
+from urllib.parse import quote_plus
 
-from ..enums.amazon_site_enum import AmazonSite as __AmazonSite
+from ..enums.amazon_enum import AmazonSite
 
 if TYPE_CHECKING:
-    from typing import Generator, Optional
+    from typing import Generator
 
 
-def is_asin(asin: Optional[str]) -> bool:
-    """判断是否符合 ASIN 格式"""
-    if asin is None or len(asin) != 10:
-        return False
-    return all(('0' <= c <= '9') or ('A' <= c <= 'Z') for c in asin)
+__all__ = [
+    #
+    'AmazonSite',
+    #
+    'is_asin',
+    #
+    'build_search_url',
+    'build_search_urls',
+    #
+    'build_detail_url',
+    #
+    'clean_product_image_url',
+]
 
 
 def build_search_url(site: str, keyword: str, page: int = 1) -> str:
     """根据站点、关键词、页码构造关键词搜索页 url"""
     if page < 1:
-        raise ValueError(f'Page must be greater than 0, page={page}')
+        raise ValueError(f'page 必须大于 0，page={page}')
 
     if page == 1:
-        return __AmazonSite.get_url(site=site) + '/s?k=' + __quote_plus(keyword)
+        return AmazonSite.get_url(site=site) + '/s?k=' + quote_plus(keyword)
     else:
-        return __AmazonSite.get_url(site=site) + '/s?k=' + __quote_plus(keyword) + '&page=' + str(page)
+        return AmazonSite.get_url(site=site) + '/s?k=' + quote_plus(keyword) + '&page=' + str(page)
 
 
 def build_search_urls(site: str, keyword: str, max_page: int = 1) -> Generator[str, None, None]:
@@ -38,13 +46,24 @@ def build_search_urls(site: str, keyword: str, max_page: int = 1) -> Generator[s
         yield build_search_url(site=site, keyword=keyword, page=page)
 
 
+asin_pattern = re.compile(r'^[A-Z0-9]{10}$')
+
+
+def is_asin(asin: str) -> bool:
+    """判断是否符合 ASIN 格式"""
+    if len(asin) != 10:
+        return False
+    # return all(('0' <= c <= '9') or ('A' <= c <= 'Z') for c in asin)
+    return asin_pattern.match(asin) is not None
+
+
 def build_detail_url(site: str, asin: str) -> str:
     """根据站点、ASIN 构造产品详情页 url"""
     if is_asin(asin):
-        return __AmazonSite.get_url(site=site) + '/dp/' + asin
+        return AmazonSite.get_url(site=site) + '/dp/' + asin
     raise ValueError(f'ASIN {asin} is not valid')
 
 
 def clean_product_image_url(url: str) -> str:
     """清理产品图 url，提取产品图的原图 url"""
-    return __re.sub(r'\._.*?_\.', '.', url)
+    return re.sub(r'\._.*?_\.', '.', url)
