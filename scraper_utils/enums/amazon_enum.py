434ad1173f2bd12f3b860a27cf61e4d6f8e4d600
Jiang <jdycty2002@163.com>
2025-02-08 17:33:25 +0800

    update amazon enum

--- scraper_utils/enums/amazon_enum.py
+++ scraper_utils/enums/amazon_enum.py
@@ -1,17 +1,22 @@
 """
-亚马逊站点枚举
+亚马逊站点相关枚举
 """
 
 from __future__ import annotations
 
-from enum import Enum as __Enum
+from enum import Enum
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import Generator
 
 
-class AmazonSite(__Enum):
+__all__ = [
+    'AmazonSite',
+]
+
+
+class AmazonSite(Enum):
     """
     亚马逊站点
 
@@ -39,28 +44,11 @@ class AmazonSite(__Enum):
         """站点对应的 url"""
         return self.value
 
-    @property
-    def cn_description(self):
-        """站点的中文描述"""
-        match self:
-            case AmazonSite.US:
-                return '美国'
-            case AmazonSite.UK:
-                return '英国'
-            case AmazonSite.DE:
-                return '德国'
-            case AmazonSite.FR:
-                return '法国'
-            case AmazonSite.IT:
-                return '意大利'
-            case AmazonSite.ES:
-                return '西班牙'
-
     @classmethod
-    def get_site(cls, site: str) -> 'AmazonSite':
+    def get_site(cls, site: str) -> AmazonSite:
         """根据 site 获取对应的站点"""
         site = site.upper()
-        if site in (s.name for s in cls.supported_sites()):
+        if any(site == s.name for s in cls.supported_sites()):
             return AmazonSite[site]
         else:
             raise ValueError(f'Site {site} not supported')
@@ -71,13 +59,7 @@ class AmazonSite(__Enum):
         return cls.get_site(site=site).url
 
     @classmethod
-    def supported_sites(cls) -> Generator['AmazonSite', None, None]:
-        """支持哪些站点"""
+    def supported_sites(cls) -> Generator[AmazonSite, None, None]:
+        """支持的站点"""
         for site in cls:
             yield site
-
-    @classmethod
-    def supported_urls(cls) -> Generator[str, None, None]:
-        """支持哪些 url"""
-        for site in cls:
-            yield site.url
