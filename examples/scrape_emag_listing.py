5072253589ec3977f0d8e7792c8d74a4bf09f9c4
kk119 <jdycty2002@163.com>
2025-02-11 10:46:47 +0800

    更新 emag listing example

--- examples/scrape_emag_listing.py
+++ examples/scrape_emag_listing.py
@@ -1,14 +1,18 @@
 """
-爬取 Emag 的 listing
+爬取 Emag 搜索页的 listing
 """
 
 import asyncio
+from random import randint, uniform
 from pathlib import Path
+from time import perf_counter
 
+from loguru import logger
 from openpyxl.workbook import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 from playwright.async_api import Page
 
+from scraper_utils.constants.time_constant import MS1000
 from scraper_utils.enums.browser_enum import ResourceType as RT
 from scraper_utils.utils.browser_util import launch_persistent_browser, create_new_page, close_browser
 from scraper_utils.utils.emag_url_util import build_search_url
@@ -19,12 +23,31 @@ from scraper_utils.utils.workbook_util import read_workbook_async, write_workboo
 async def parse_search(page: Page) -> list[str]:
     """解析搜索页"""
     result: list[str] = list()
-    item_cards = page.locator(r'//div[@class="card-item card-standard js-product-data js-card-clickable "]')
-    for item_card in await item_cards.all():
-        top_favourite = item_card.locator(r'//div[@class="card-v2-badge-cmp badge commercial-badge"]')
-        if await top_favourite.count() > 0:
-            pass
-        # TODO
+
+    wheel_start_time = perf_counter()
+    while True:  # 模拟鼠标滚轮向下滚动网页
+        item_card_tags = page.locator(r'//div[@class="card-item card-standard js-product-data js-card-clickable "]')
+        if await item_card_tags.count() >= 67:
+            break
+        await page.mouse.wheel(delta_y=randint(50, 150), delta_x=0)
+        await page.wait_for_timeout(uniform(0, 0.5) * MS1000)
+        if perf_counter() - wheel_start_time >= 10:  # 10 秒后数量还不够，就有多少爬多少
+            break
+
+    logger.debug('定位到 ' + str(await item_card_tags.count()) + ' 个 item_card_tags')
+    for item_card_tag in await item_card_tags.all():
+        top_favourite_tag = item_card_tag.locator(r'//span[text()="Top Favorite"]')
+        if await top_favourite_tag.count() > 0:
+            logger.debug('定位到 1 个 top_favourite_tag')
+            item_title_tag = item_card_tag.locator(r'//a[@data-zone="title"]')
+            if await item_title_tag.count() > 0:
+                logger.debug('定位到 1 个 item_title_tag')
+                item_title = await item_title_tag.inner_text(timeout=MS1000)
+                logger.debug(f'找到 1 个 listing: {item_title}')
+                result.append(item_title)
+
+    logger.debug(f'总计找到 {len(result)} 个 listing')
+    return result
 
 
 async def scrape_search(CWD: Path):
@@ -34,11 +57,12 @@ async def scrape_search(CWD: Path):
         user_data_dir=Path.cwd().joinpath('temp/chrome_data'),
         channel='chrome',
         headless=False,
+        timeout=60 * MS1000,
     )
-    abort_resources = (RT.MEDIA, RT.IMAGE)
+    abort_resources = (RT.MEDIA, RT.IMAGE)  # 屏蔽图片、音视频资源
 
     workbook = await read_workbook_async(CWD.joinpath('temp/emag_keyword.xlsx'), read_only=True)
-    worksheet = workbook['Wg wartości sprzedaży']
+    worksheet = workbook['Sheet1']
 
     for index in range(4, 62):
         keyword = str(worksheet.cell(index, 4).value)
@@ -47,7 +71,9 @@ async def scrape_search(CWD: Path):
         page = await create_new_page(stealth=True, abort_resources=abort_resources)
         await page.goto(search_url)
 
+        logger.info(f'正在爬取：{keyword}')
         listings = await parse_search(page=page)
+        await page.close()
         await write_json_async(
             file=CWD.joinpath(f'temp/emag_listings/{index}.json'),
             data={
@@ -56,7 +82,7 @@ async def scrape_search(CWD: Path):
             },
         )
 
-        await asyncio.sleep(30)
+        await asyncio.sleep(30)  # 60 秒的延时
 
     await close_browser()
 
@@ -70,7 +96,8 @@ async def concat_search(CWD: Path):
         for index, product in enumerate(data['listings'], start=1):
             sheet.cell(row=index, column=1, value=product)
 
-    await write_workbook_async(file=CWD.joinpath('temp/emag_listings.xlsx'), workbook=workbook)
+    result_path = await write_workbook_async(file=CWD.joinpath('temp/emag_listings.xlsx'), workbook=workbook)
+    logger.success(f'程序结束，结果保存至：{result_path}')
 
 
 if __name__ == '__main__':
