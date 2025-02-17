4e821abb990097422cf419cd09db1371d4618096
kk119 <jdycty2002@163.com>
2025-02-17 23:09:24 +0800

    更新 scrape_emag_keyword.py

--- examples/scrape_emag_keyword.py
+++ examples/scrape_emag_keyword.py
@@ -3,11 +3,11 @@
 """
 
 import asyncio
+import re
 from asyncio.exceptions import CancelledError
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 from random import randint, uniform
-import re
 from re import Pattern
 from time import perf_counter
 from typing import Optional, Self, Sequence
@@ -15,16 +15,16 @@ from typing import Optional, Self, Sequence
 from aiohttp import ClientResponseError, ClientSession
 from fake_http_header import FakeHttpHeader
 from loguru import logger
+from openpyxl.styles.alignment import Alignment
 from openpyxl.styles.fills import PatternFill
 from openpyxl.styles.fonts import Font
-from openpyxl.styles.alignment import Alignment
 from openpyxl.workbook import Workbook
-from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.worksheet.hyperlink import Hyperlink
+from openpyxl.worksheet.worksheet import Worksheet
 from playwright.async_api import Page
 
 from scraper_utils.constants.time_constant import MS1000
-from scraper_utils.enums.browser_enum import ResourceType as RT
+from scraper_utils.enums.browser_enum import ResourceType
 from scraper_utils.exceptions.browser_exception import PlaywrightError
 from scraper_utils.utils.browser_util import launch_persistent_browser, close_browser, create_new_page
 from scraper_utils.utils.emag_url_util import build_search_urls, build_product_url, clean_product_image_url
@@ -33,17 +33,16 @@ from scraper_utils.utils.workbook_util import (
     read_workbook_sync,
     write_workbook_sync,
     string_column_to_integer_column as s2i,
-    integer_column_to_string_column as i2s,
 )
 
 
 @dataclass
 class ItemCardParseResult:
     pnk: str
-    top_favorite: bool  # 是否带有 top 标志
-    image_url: Optional[str]  # 产品图 url
-    review_count: Optional[int]  # 评论数
-    price: Optional[float]  # 价格
+    top_favorite: bool = False  # 是否带有 top 标志
+    image_url: Optional[str] = None  # 产品图 url
+    review_count: Optional[int] = None  # 评论数
+    price: Optional[float] = None  # 价格
 
     def __hash__(self):
         return hash(self.pnk)
@@ -67,7 +66,7 @@ class ItemCardParseResult:
 @dataclass
 class KeywordSearchResults:
     keyword: str
-    products: list[ItemCardParseResult]
+    products: list[ItemCardParseResult] = field(default_factory=list)
 
     def __add__(self, other: Self):
         """合并两个结果，并去除其中的重复产品"""
@@ -170,39 +169,39 @@ async def start_scrape(cwd: Path, target_rows: Sequence[int]):
             one_keyword_result_sheet.column_dimensions['B'].width = 40
 
             # 遍历爬取结果，插入到工作表中
-            for row_result, product_result in enumerate(one_keyword_result.products, start=2):
+            for result_row, product_result in enumerate(one_keyword_result.products, start=2):
                 # 设置该行的高度
-                one_keyword_result_sheet.row_dimensions[row_result].height = 40
+                one_keyword_result_sheet.row_dimensions[result_row].height = 40
 
                 # 产品详情页 url
-                one_keyword_result_sheet.cell(row=row_result, column=1, value=product_result.url)
-                one_keyword_result_sheet.cell(row=row_result, column=1).alignment = align
+                one_keyword_result_sheet.cell(row=result_row, column=1, value=product_result.url)
+                one_keyword_result_sheet.cell(row=result_row, column=1).alignment = align
 
                 # 产品图
                 if product_result.image_url is not None:
                     # TODO 插入图片
-                    one_keyword_result_sheet.cell(row=row_result, column=2, value=product_result.origin_image_url)
-                    one_keyword_result_sheet.cell(row=row_result, column=2).alignment = align
-                    one_keyword_result_sheet.cell(row=row_result, column=2).hyperlink = Hyperlink(
+                    one_keyword_result_sheet.cell(row=result_row, column=2, value=product_result.origin_image_url)
+                    one_keyword_result_sheet.cell(row=result_row, column=2).alignment = align
+                    one_keyword_result_sheet.cell(row=result_row, column=2).hyperlink = Hyperlink(
                         ref=product_result.origin_image_url,
                         target=product_result.origin_image_url,
                     )
-                    one_keyword_result_sheet.cell(row=row_result, column=2).font = hyperlink_font
-                    one_keyword_result_sheet.cell(row=row_result, column=2)
+                    one_keyword_result_sheet.cell(row=result_row, column=2).font = hyperlink_font
+                    one_keyword_result_sheet.cell(row=result_row, column=2)
 
                 # Top Favorite
-                one_keyword_result_sheet.cell(row=row_result, column=3, value=product_result.top_favorite)
-                one_keyword_result_sheet.cell(row=row_result, column=3).alignment = align
+                one_keyword_result_sheet.cell(row=result_row, column=3, value=str(product_result.top_favorite))
+                one_keyword_result_sheet.cell(row=result_row, column=3).alignment = align
 
                 # 评论数
                 if product_result.review_count is not None:
-                    one_keyword_result_sheet.cell(row=row_result, column=4, value=product_result.review_count)
-                    one_keyword_result_sheet.cell(row=row_result, column=4).alignment = align
+                    one_keyword_result_sheet.cell(row=result_row, column=4, value=product_result.review_count)
+                    one_keyword_result_sheet.cell(row=result_row, column=4).alignment = align
 
                 # 价格
                 if product_result.price is not None:
-                    one_keyword_result_sheet.cell(row=row_result, column=5, value=product_result.price)
-                    one_keyword_result_sheet.cell(row=row_result, column=5).alignment = align
+                    one_keyword_result_sheet.cell(row=result_row, column=5, value=product_result.price)
+                    one_keyword_result_sheet.cell(row=result_row, column=5).alignment = align
 
     # Ctrl + C 不会退出程序，而是停止爬取并运行下面的结果保存
     except CancelledError:
@@ -303,7 +302,7 @@ async def parse_search_page(
             price_tag = item_card_tag.locator('//p[@class="product-new-price"]')
             if await price_tag.count() > 0:
                 price_text = await price_tag.first.inner_text(timeout=MS1000)
-                if len(price) > 0:
+                if len(price_text) > 0:
                     price = float(price_pattern.sub('', price_text).replace(',', '.'))
             one_item.price = price
 
@@ -316,7 +315,7 @@ async def parse_search_page(
 
 async def download_product_image(cwd: Path, url: str, pnk: str, client: ClientSession, fake_header: dict[str, str]):
     """下载产品图，用 pnk 做图片的文件名"""
-    img_format = ''  # TODO
+    img_format = ''  # TODO 如何确定文件后缀名？
     async with client.get(
         url=url,
         headers=fake_header,
@@ -327,7 +326,7 @@ async def download_product_image(cwd: Path, url: str, pnk: str, client: ClientSe
             logger.error(cre)
         else:
             content = await response.read()
-            await write_bytes_async(file=cwd.joinpath(f'temp/emag_product_images/{pnk}.{img_format}'))
+            await write_bytes_async(file=cwd.joinpath(f'temp/emag_product_images/{pnk}.{img_format}'), data=content)  # TODO 这个 data 怎么传？
 
 
 if __name__ == '__main__':
@@ -337,7 +336,7 @@ if __name__ == '__main__':
         start_time = perf_counter()
         logger.info('程序启动')
 
-        abort_res: set[RT] = (RT.MEDIA, RT.IMAGE, RT.STYLESHEET, RT.FONT)
+        abort_res: tuple[ResourceType, ...] = (ResourceType.MEDIA, ResourceType.IMAGE, ResourceType.STYLESHEET, ResourceType.FONT)
         await launch_persistent_browser(
             user_data_dir=cwd.joinpath('temp/chrome_data'),
             executable_path=r'C:\Program Files\Google\Chrome\Application\chrome.exe',
