bb8183428d2a8a85b8fd044c9a7608992d197490
kk119 <jdycty2002@163.com>
2025-02-18 18:26:29 +0800

    更新 scrape_emag_keyword 示例

--- examples/scrape_emag_keyword.py
+++ examples/scrape_emag_keyword.py
@@ -3,16 +3,17 @@
 """
 
 import asyncio
-import re
+from asyncio.locks import Semaphore
 from asyncio.exceptions import CancelledError
 from dataclasses import dataclass, field
 from pathlib import Path
 from random import randint, uniform
+import re
 from re import Pattern
 from time import perf_counter
 from typing import Optional, Self, Sequence
 
-from aiohttp import ClientResponseError, ClientSession
+from aiohttp import ClientResponseError, ClientSession, ClientTimeout
 from fake_http_header import FakeHttpHeader
 from loguru import logger
 from openpyxl.styles.alignment import Alignment
@@ -28,12 +29,17 @@ from scraper_utils.enums.browser_enum import ResourceType
 from scraper_utils.exceptions.browser_exception import PlaywrightError
 from scraper_utils.utils.browser_util import launch_persistent_browser, close_browser, create_new_page
 from scraper_utils.utils.emag_url_util import build_search_urls, build_product_url, clean_product_image_url
-from scraper_utils.utils.file_util import write_bytes_async
+from scraper_utils.utils.file_util import write_bytes_async, path_exists
+from scraper_utils.utils.image_util import read_image_async, resize_image
 from scraper_utils.utils.workbook_util import (
     read_workbook_sync,
     write_workbook_sync,
     string_column_to_integer_column as s2i,
+    insert_image,
 )
+
+
+product_image_ext_pattern = re.compile(r'/images/[0-9a-z_]+\.([a-z]+)')  # 获取图片拓展名
 
 
 @dataclass
@@ -53,13 +59,53 @@ class ItemCardParseResult:
     @property
     def url(self) -> str:
         """产品详情页 url"""
-        return build_product_url(pnk=self.pnk)
+        if getattr(self, '__url', None) is not None:  # 缓存
+            return self.__url
+
+        self.__url = build_product_url(pnk=self.pnk)
+        return self.__url
 
     @property
     def origin_image_url(self) -> Optional[str]:
         """产品图原图 url"""
+        if getattr(self, '__origin_image_url', None) is not None:  # 缓存
+            return self.__origin_image_url
+
         if self.image_url is not None:
-            return clean_product_image_url(url=self.image_url)
+            self.__origin_image_url = clean_product_image_url(url=self.image_url)
+            return self.__origin_image_url
+
+        return None
+
+    @property
+    def image_ext(self) -> Optional[str]:
+        """产品图的文件拓展名"""
+        if getattr(self, '__image_ext', None) is not None:  # 缓存
+            return self.__image_ext
+
+        if self.origin_image_url is not None:
+            image_ext_match = product_image_ext_pattern.search(self.origin_image_url)
+            if image_ext_match is not None:
+                image_ext = image_ext_match.group(1)
+                if image_ext is None or len(image_ext) == 0:
+                    raise ValueError('image_ext 为空')
+                if image_ext.lower() == 'jpg':
+                    image_ext = 'jpeg'
+                self.__image_ext = image_ext
+                return self.__image_ext
+
+        return None
+
+    @property
+    def image_save_path(self) -> Optional[Path]:
+        """产品图下载后保存的位置"""
+        if getattr(self, '__image_save_path', None) is not None:  # 缓存
+            return self.__image_save_path
+
+        if self.image_ext is not None:
+            self.__image_save_path = cwd.joinpath(f'temp/emag_product_images/{self.pnk}.{self.image_ext}')
+            return self.__image_save_path
+
         return None
 
 
@@ -95,7 +141,7 @@ async def start_scrape(cwd: Path, target_rows: Sequence[int]):
     red_bold_font = Font(color='FF0000', bold=True)  # 红字、加粗，标题栏用
     yellow_fill_color = PatternFill(fill_type='solid', fgColor='FFFF00')  # 黄色前景色，标题栏用
     hyperlink_font = Font(color='0000FF', underline='single')  # 超链接字体，蓝色下划线，超链接单元格用
-    align = Alignment(wrap_text=True, horizontal='center', vertical='center')  # 居中、自动换行
+    center_align = Alignment(wrap_text=True, horizontal='center', vertical='center')  # 居中、自动换行
 
     # 准备用于存储结果的工作簿
     result_workbook = Workbook()
@@ -103,16 +149,17 @@ async def start_scrape(cwd: Path, target_rows: Sequence[int]):
 
     ##########
 
-    # 遍历关键词用于爬取
-    pnk_pattern = re.compile(r'/pd/([0-9A-Z]{9})(/|$)')
-    review_count_pattern = re.compile(r'\((\d+)\)')
-    price_pattern = re.compile(r'[^\d,]')
+    # 解析页面用的正则表达式
+    pnk_pattern = re.compile(r'/pd/([0-9A-Z]{9})(/|$)')  # pnk
+    review_count_pattern = re.compile(r'\((\d+)\)')  # 评论数
+    price_pattern = re.compile(r'[^\d,]')  # 价格
 
+    # 遍历关键词用于爬取
     total_result: list[KeywordSearchResults] = list()  # 整个关键词表格的搜索结果
 
     # 下载产品图用
     client_session = ClientSession()
-    fake_header = FakeHttpHeader()
+    fake_header = FakeHttpHeader(browser='chrome')
 
     try:
         for row in target_rows:
@@ -121,7 +168,7 @@ async def start_scrape(cwd: Path, target_rows: Sequence[int]):
                 continue
             keyword = str(keyword)
             one_keyword_result = KeywordSearchResults(keyword=keyword, products=list())  # 单个关键词的爬取结果
-            logger.info(f'开始爬取关键词 "{keyword}"')
+            logger.info(f'开始爬取第 {row} 行关键词 "{keyword}"')
             # 爬取关键词搜索结果
             for url in build_search_urls(keyword=keyword, max_page=1):
                 page = await create_new_page()
@@ -144,64 +191,105 @@ async def start_scrape(cwd: Path, target_rows: Sequence[int]):
                     await asyncio.sleep(30 + randint(0, 30))  # 每爬完一个 url 就随机等待 30-60 秒
                     await page.close()
             total_result.append(one_keyword_result)
-            logger.success(f'关键词 "{keyword}" 爬取结束')
 
             ##########
+
+            # TODO 应该把下面这部分的提取成单独的方法
 
             # 每爬完一个关键词就写入一个工作表
             one_keyword_result_sheet: Worksheet = result_workbook.create_sheet(
                 title=keyword if len(keyword) < 28 else keyword[:28] + '...',
             )  # sheet_name 有长度上限，超出部分就用省略号代替
 
+            one_keyword_result_sheet['A1'] = '关键词'
+            one_keyword_result_sheet['B1'] = keyword
+
             # 创建标题行
-            one_keyword_result_sheet['A1'] = '产品链接'
-            one_keyword_result_sheet['B1'] = '产品图'
-            one_keyword_result_sheet['C1'] = '是否有 Top Favorite'
-            one_keyword_result_sheet['D1'] = '评论数'
-            one_keyword_result_sheet['E1'] = '价格'
+            one_keyword_result_sheet['A2'] = '产品链接'
+            one_keyword_result_sheet['B2'] = '产品图'
+            one_keyword_result_sheet['C2'] = '是否有 Top Favorite'
+            one_keyword_result_sheet['D2'] = '评论数'
+            one_keyword_result_sheet['E2'] = '价格'
 
             for col in range(1, 6):  # 设置标题行的样式
-                one_keyword_result_sheet.cell(1, col).alignment = align
-                one_keyword_result_sheet.cell(1, col).font = red_bold_font
-                one_keyword_result_sheet.cell(1, col).fill = yellow_fill_color
+                one_keyword_result_sheet.cell(2, col).alignment = center_align
+                one_keyword_result_sheet.cell(2, col).font = red_bold_font
+                one_keyword_result_sheet.cell(2, col).fill = yellow_fill_color
 
-            # 设置图片列的宽度
-            one_keyword_result_sheet.column_dimensions['B'].width = 40
+            # 设置各列的宽度
+            for col in ['A', 'B', 'C', 'D', 'E']:
+                one_keyword_result_sheet.column_dimensions[col].width = int(120 / 7)  # 16.04 字符 ≈ 100 磅
+
+            ##########
+
+            # 并发下载产品图
+            download_product_image_semaphore = Semaphore(8)  # 并发数限制
+
+            download_product_image_tasks = [
+                download_product_image_task(
+                    image_url=pr.origin_image_url,
+                    save_path=pr.image_save_path,
+                    client_session=client_session,
+                    fake_header=fake_header.as_header_dict(),
+                    semaphore=download_product_image_semaphore,
+                )
+                for pr in one_keyword_result.products
+                if pr.image_save_path is not None
+            ]
+            logger.info(f'下载 "{keyword}" 的产品图')
+            await asyncio.gather(*download_product_image_tasks)  # 并发下载产品图
+
+            ##########
+
+            logger.info(f'将 "{keyword}" 的爬取结果保存成工作表')
 
             # 遍历爬取结果，插入到工作表中
-            for result_row, product_result in enumerate(one_keyword_result.products, start=2):
-                # 设置该行的高度
-                one_keyword_result_sheet.row_dimensions[result_row].height = 40
-
+            for result_row, pr in enumerate(one_keyword_result.products, start=3):
                 # 产品详情页 url
-                one_keyword_result_sheet.cell(row=result_row, column=1, value=product_result.url)
-                one_keyword_result_sheet.cell(row=result_row, column=1).alignment = align
+                one_keyword_result_sheet.cell(row=result_row, column=1, value=pr.url)
+                one_keyword_result_sheet.cell(row=result_row, column=1).alignment = center_align
+                one_keyword_result_sheet.cell(row=result_row, column=1).font = hyperlink_font
+                one_keyword_result_sheet.cell(row=result_row, column=1).hyperlink = Hyperlink(ref=pr.url, target=pr.url)
 
                 # 产品图
-                if product_result.image_url is not None:
-                    # TODO 插入图片
-                    one_keyword_result_sheet.cell(row=result_row, column=2, value=product_result.origin_image_url)
-                    one_keyword_result_sheet.cell(row=result_row, column=2).alignment = align
+                if pr.origin_image_url is not None:
+                    one_keyword_result_sheet.cell(row=result_row, column=2, value=pr.origin_image_url)
+                    one_keyword_result_sheet.cell(row=result_row, column=2).alignment = center_align
                     one_keyword_result_sheet.cell(row=result_row, column=2).hyperlink = Hyperlink(
-                        ref=product_result.origin_image_url,
-                        target=product_result.origin_image_url,
+                        ref=pr.origin_image_url,
+                        target=pr.origin_image_url,
                     )
                     one_keyword_result_sheet.cell(row=result_row, column=2).font = hyperlink_font
-                    one_keyword_result_sheet.cell(row=result_row, column=2)
+
+                    # 检查图片是否存在，存在则插入到工作表
+                    if path_exists(pr.image_save_path):
+                        one_keyword_result_sheet.row_dimensions[result_row].height = int(120 * 0.75)
+
+                        product_image = await read_image_async(file=pr.image_save_path)
+                        product_image = resize_image(image=product_image, height=120, width=120)
+                        insert_image(
+                            sheet=one_keyword_result_sheet,
+                            image=product_image,
+                            row=result_row,
+                            column='B',
+                            image_format=pr.image_ext,
+                        )
 
                 # Top Favorite
-                one_keyword_result_sheet.cell(row=result_row, column=3, value=str(product_result.top_favorite))
-                one_keyword_result_sheet.cell(row=result_row, column=3).alignment = align
+                one_keyword_result_sheet.cell(row=result_row, column=3, value=str(pr.top_favorite))
+                one_keyword_result_sheet.cell(row=result_row, column=3).alignment = center_align
 
                 # 评论数
-                if product_result.review_count is not None:
-                    one_keyword_result_sheet.cell(row=result_row, column=4, value=product_result.review_count)
-                    one_keyword_result_sheet.cell(row=result_row, column=4).alignment = align
+                if pr.review_count is not None:
+                    one_keyword_result_sheet.cell(row=result_row, column=4, value=pr.review_count)
+                    one_keyword_result_sheet.cell(row=result_row, column=4).alignment = center_align
 
                 # 价格
-                if product_result.price is not None:
-                    one_keyword_result_sheet.cell(row=result_row, column=5, value=product_result.price)
-                    one_keyword_result_sheet.cell(row=result_row, column=5).alignment = align
+                if pr.price is not None:
+                    one_keyword_result_sheet.cell(row=result_row, column=5, value=pr.price)
+                    one_keyword_result_sheet.cell(row=result_row, column=5).alignment = center_align
+
+            logger.success(f'"{keyword}" 爬取结束')
 
     # Ctrl + C 不会退出程序，而是停止爬取并运行下面的结果保存
     except CancelledError:
@@ -214,7 +302,7 @@ async def start_scrape(cwd: Path, target_rows: Sequence[int]):
             file=cwd.joinpath(f'temp/scrape_emag_keyword_结果_{min(target_rows)}-{max(target_rows)}.xlsx'),
             workbook=result_workbook,
         )
-        logger.success(f'结果已保存至 "{result_path}"')
+        logger.success(f'程序结束，结果已保存至 "{result_path}"')
 
 
 async def parse_search_page(
@@ -313,20 +401,41 @@ async def parse_search_page(
     return result
 
 
-async def download_product_image(cwd: Path, url: str, pnk: str, client: ClientSession, fake_header: dict[str, str]):
-    """下载产品图，用 pnk 做图片的文件名"""
-    img_format = ''  # TODO 如何确定文件后缀名？
-    async with client.get(
-        url=url,
-        headers=fake_header,
-    ) as response:
-        try:
-            response.raise_for_status()
-        except ClientResponseError as cre:
-            logger.error(cre)
-        else:
-            content = await response.read()
-            await write_bytes_async(file=cwd.joinpath(f'temp/emag_product_images/{pnk}.{img_format}'), data=content)  # TODO 这个 data 怎么传？
+async def save_keyword_result_to_sheet():
+    """保存单个关键词的爬取结果到工作表中"""
+    # TODO
+
+
+async def download_product_image_task(
+    image_url: str,
+    save_path: Path,
+    client_session: ClientSession,
+    fake_header: dict[str, str],
+    semaphore: Semaphore,
+) -> None:
+    """并发下载单个 sheet 内的全部产品图"""
+    if path_exists(path=save_path):
+        # logger.debug(f'产品图已存在 "{save_path}"')
+        return
+    else:
+        async with semaphore:
+            # logger.debug(f'下载产品图 "{image_url}"')
+            async with client_session.get(
+                url=image_url,
+                headers=fake_header,
+                timeout=ClientTimeout(total=30),
+            ) as response:
+                try:
+                    response.raise_for_status()
+                except ClientResponseError as cre:
+                    logger.error(cre)
+                except TimeoutError as te:
+                    logger.error(te)
+                except CancelledError as ce:
+                    logger.error(ce)
+                else:
+                    content = await response.read()
+                    await write_bytes_async(file=save_path, data=content)
 
 
 if __name__ == '__main__':
@@ -336,18 +445,23 @@ if __name__ == '__main__':
         start_time = perf_counter()
         logger.info('程序启动')
 
-        abort_res: tuple[ResourceType, ...] = (ResourceType.MEDIA, ResourceType.IMAGE, ResourceType.STYLESHEET, ResourceType.FONT)
+        abort_res: tuple[ResourceType, ...] = (
+            ResourceType.MEDIA,
+            ResourceType.IMAGE,
+            ResourceType.STYLESHEET,
+            ResourceType.FONT,
+        )
         await launch_persistent_browser(
             user_data_dir=cwd.joinpath('temp/chrome_data'),
             executable_path=r'C:\Program Files\Google\Chrome\Application\chrome.exe',
             channel='chrome',
-            headless=False,
+            # headless=False,
             timeout=60 * MS1000,
             stealth=True,
             abort_resources=abort_res,
         )
 
-        target_rows = list(_ for _ in range(3, 149 + 1))
+        target_rows = list(range(106, 149 + 1))
         await start_scrape(cwd=cwd, target_rows=target_rows)
 
         await close_browser()
